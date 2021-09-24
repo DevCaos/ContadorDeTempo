ced6a5b4fb333075b345f2e08ddd17214260b4b0
DevCaos <90732269+DevCaos@users.noreply.github.com>
2021-09-24 17:57:38 -0300

    Uptade script.js
    corrigi bug da idade negativa

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ function clica() {
     let horaFinal = hora.value
     let minutoFinal = horaFinal * 30 * 12 * 60 * idadeFinal
 
+    if(nomeFinal == '') {
+    res.innerHTML += 'Faltam dados!'
+} else
 
+if (idadeFinal < 0) {
+    res.innerHTML += 'Digite uma idade válida!'
+} else {
     res.innerHTML += `Olá, ${nomeFinal} sua idade é ${idadeFinal} anos. Você gastou ${minutoFinal} minutos dormindo durante ${idadeFinal} anos da sua vida.`
-
+}
 }
